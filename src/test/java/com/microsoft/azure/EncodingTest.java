c36822f30cd14cc683851060b9ded95ffc6328d1
Mona Sun (CSI Interfusion Inc) <v-sunxi@microsoft.com>
2019-03-18 14:59:31 +0800

    update test files for test runner

---
+++ src/test/java/com/microsoft/azure/EncodingTest.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.azure;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.*;
+
+
+public class EncodingTest {
+   @Test
+   public void test1(){
+       assertTrue(true);
+       System.out.println("你好！");
+   }
+}
